445e9ad2b54c189719ce8e633eb315dd486acea8
abastin99 <45099001+abastin99@users.noreply.github.com>
2020-12-07 23:08:00 +0000

    Initial Commit

---
+++ Project/Test/abc.py
@@ -0,0 +1,13 @@
+import pcapkit
+import os
+
+
+os.getcwd()
+relativePath = 'test.pcap' # Relative directory something like '../test.pcap'
+fullPath = os.path.join(os.getcwd(),relativePath) # Produces something like '/home/hallandspur/Documents/test.pcap'
+extraction = pcapkit.extract(fin=fullPath, nofile=False, fout='out.plist', format='plist')  
+frame0 = extraction.frame[0]
+# check if IP in this frame, otherwise ProtocolNotFound will be raised
+flag = pcapkit.IP in frame0
+tcp = frame0[pcapkit.IP] if flag else None
+#print(tcp)
