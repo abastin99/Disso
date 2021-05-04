c7f289193c08f83cb10b6edb71f1b0d589acd955
abastin99 <45099001+abastin99@users.noreply.github.com>
2021-05-04 22:33:09 +0100

    tables

--- Project/Test/abc.py
+++ Project/Test/abc.py
@@ -1,13 +1,31 @@
 import pcapkit
 import os
-
+from prettytable import PrettyTable
 
 os.getcwd()
-relativePath = 'Project\\resources\\test.pcap' # Relative directory something like '../test.pcap'
+relativePath = 'Project\\resources\\ipp.pcap' # Relative directory something like '../test.pcap'
 fullPath = os.path.join(os.getcwd(),relativePath) # Produces something like '/home/hallandspur/Documents/test.pcap'
-extraction = pcapkit.extract(fin=fullPath, nofile=False, fout='out.plist', format='plist')  
-frame0 = extraction.frame[0]
-# check if IP in this frame, otherwise ProtocolNotFound will be raised
-flag = pcapkit.IP in frame0
-tcp = frame0[pcapkit.IP] if flag else None
-#print(tcp)
+extraction = pcapkit.extract(fin=fullPath, nofile=True) 
+totalFrames = len(extraction.frame) 
+print(totalFrames)
+
+out = PrettyTable()
+out.field_names = ["Source IP", "Destination IP", "Protocol"]
+
+# check if IP in this frame, otherwise don't print
+for x in range(totalFrames):
+    flag = pcapkit.IP in extraction.frame[x]
+    frameInfo = extraction.frame[x][pcapkit.IP] if flag else None
+    frameTime = extraction.frame[x].info.time  #frame_info
+    if x >= 1:
+        previousTime = extraction.frame[x - 1].info.time
+        time = frameTime - previousTime
+        print(time)
+    if frameInfo != None:
+        sourceIP = frameInfo.src
+        destinationIP = frameInfo.dst
+        protocolUsed = frameInfo.protocol
+        out.add_row([sourceIP,destinationIP, protocolUsed])
+        
+        
+print(out)   
